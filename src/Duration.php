f976e3dbb1642725b0440fcd5d54e6024d055c50
clvarley <conorlukevarley@gmail.com>
2021-10-29 14:23:23 +0100

    Allowed duration to be of varying precision

--- src/Duration.php
+++ src/Duration.php
@@ -10,19 +10,67 @@ namespace Clvarley\Bench;
 Class Duration
 {
 
-    /**
-     * @var int $seconds
-     */
-    public $seconds = 0;
+    const PRECISION_SECONDS = 1000 ^ 0;
+    const PRECISION_MILLISECONDS = 1000 ^ 1;
+    const PRECISION_MICROSECONDS = 1000 ^ 2;
 
     /**
-     * @var int $milliseconds
+     * @var float $value
      */
-    public $milliseconds = 0;
+    private $value;
 
     /**
-     * @var int $microseconds
+     * @psalm-var self::PRECISION_* $precision
+     * @var int $precision
      */
-    public $microseconds = 0;
+    private $precision;
 
+    /**
+     * Create a new duration value using
+     *
+     * @psalm-param self::PRECISION_* $precision
+     * @param float $value   Duration length
+     * @param int $precision Value precision
+     */
+    public function __construct( float $value, int $precision = self::PRECISION_MICROSECONDS )
+    {
+        $this->value = $value;
+        $this->precision = $precision;
+    }
+
+
+    /**
+     * Returns the duration in seconds
+     */
+    public function getSeconds() : float
+    {
+        return $this->value * $this->precision;
+    }
+
+    /**
+     * Returns the duration in milliseconds
+     */
+    public function getMilliseconds() : float
+    {
+        return $this->value * $this->multiplier( self::PRECISION_MILLISECONDS );
+    }
+
+    /**
+     * Returns the duration in microseconds
+     */
+    public function getMicroseconds() : float
+    {
+        return $this->value * $this->multiplier( self::PRECISION_MICROSECONDS );
+    }
+
+    /**
+     * Get the multiplier required to convert to a given precision
+     *
+     * @psalm-param self::PRECISION_* $target
+     * @param int $target Target precision
+     */
+    public function multiplier( int $target ) : int
+    {
+        return $target / $this->precision;
+    }
 }
